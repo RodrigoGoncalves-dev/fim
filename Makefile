6b271f24c4c7f86f8413fdb248c2a50dde0283c0
Rodrigo Silva <rodrigo.goncalves2001r@gmail.com>
2026-04-18 14:28:12 -0300

    criando a primeira versao

---
+++ Makefile
@@ -0,0 +1,2 @@
+fim: fim.c
+	$(CC) fim.c -o fim -I. -Wall -Wextra -pedantic -std=c99
---
+++ defines.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+
+/*** Defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+#define ABUF_INIT {NULL, 0}
+#define FIM_VERSION "v0.0.1"
+
+#endif // !HEADER_H
